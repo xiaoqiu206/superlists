06a61bc35bc5d4fd40dc5dc5ab7d1eaecb10bff6
xiaoqiu <xiaoqiu206@163.com>
2015-11-17 22:27:01 +0800

    功能测试,单元测试,home.html,tests.py和views.py有变动

--- functional_tests.py
+++ functional_tests.py
@@ -43,14 +43,27 @@ class NewVistorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.sned_keys(Keys.ENTER)
+
+        # 页面再次更新,清单中显示了这2个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # 伊迪丝想知道这个网站是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+
         self.fail('Finish the test!')
 
-
+        # 她访问哪个URL,发现待办事项还在
 if __name__ == '__main__':
     unittest.main()
