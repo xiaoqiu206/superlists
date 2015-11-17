c42bced20d98fc05408ae3fdf7fb0daf0e2509eb
xiaoqiu <xiaoqiu206@163.com>
2015-11-17 11:39:54 +0800

    功能测试修改后能输入待办事项

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
+
+def home_page(request):
+    return HttpResponse("""<html>
+            <head>
+                <title>To-Do list</title>
+            </head>
+        </html>""")
