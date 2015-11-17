366263ea3c731045f9f32a378b9765ba66b3f397
xiaoqiu <xiaoqiu206@163.com>
2015-11-17 23:38:40 +0800

    创建Item模型及相关数据库迁移文件

--- functional_tests.py
+++ functional_tests.py
@@ -13,10 +13,14 @@ class NewVistorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
@@ -40,10 +44,7 @@ class NewVistorTest(unittest.TestCase):
         # 她按回车键后,页面更新了
         # 待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框,可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
@@ -53,11 +54,8 @@ class NewVistorTest(unittest.TestCase):
         inputbox.sned_keys(Keys.ENTER)
 
         # 页面再次更新,清单中显示了这2个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
